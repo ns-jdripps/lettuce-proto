f1e85d7e1907507e0b9ebf2892e74cf45ed3f7de
Celestine <ckao@narrativescience.com>
2017-12-08 16:40:54 -0600

    adding a wait example

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,6 +1,11 @@
 from lettuce import *
 from lettuce import step
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
 
+def waitForLoad(selector):
+    wait(world.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
 
 @step('I go to "([^"]*)"')
 def go_to_site(step, url):
@@ -8,7 +13,9 @@ def go_to_site(step, url):
 
 @step('I search "([^"]*)"')
 def input_search_term(step, search_term):
-    search_input = world.browser.find_element_by_css_selector(".gsfi")
+    search_input_selector = ".gsfi"
+    waitForLoad(search_input_selector)
+    search_input = world.browser.find_element_by_css_selector(search_input_selector)
     search_input.send_keys(search_term)
     search_input.submit()
 
@@ -19,3 +26,5 @@ def results_should_contain(step, result):
 @step('I should see "([^"]*)"')
 def should_see(step, welcome_text):
     assert world.browser.find_element_by_xpath("//*[text()='%s']" % welcome_text)
+
+
